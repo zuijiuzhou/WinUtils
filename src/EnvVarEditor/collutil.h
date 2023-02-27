914e2f9176d348ab76e41d8346d220a8a1538c83
zuijiuzhou <yong-zh@qq.com>
2023-02-27 23:14:04 +0800

    ---

---
+++ src/EnvVarEditor/collutil.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <list>
+
+template<typename T> bool coll_contains();
+
+
+template<typename T> bool coll_contains(){
+    std::vector<int > ints;
+    std::find();
+}
